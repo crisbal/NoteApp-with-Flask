e70736259e990921c3a7f41a7f84e87163b36f1b
cris9696 <bld.cris.96@gmail.com>
2014-05-02 15:45:54 +0200

    I now have login logout register

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ from Models import *
 app = Flask(__name__)
 
 
+#ROUTING
+
 @app.route('/')
 def index():
     return render_template('basic.html')
@@ -14,34 +16,76 @@ def index():
 def notes():
     if is_logged_in():
         return render_template('notes.html')
-    else
+    else:
         return  redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if is_logged_in():
+        return redirect(url_for('notes'))
+
     if request.method == 'POST':
-        try:
             if is_logged_in():
                 return redirect(url_for('notes'))
+            elif request.form.get('email', None) and request.form.get('password', None):
+                user = user_exists(request.form.get('email'))
+                if user:
+                    session_login(user)
+                    return redirect(url_for('notes'))
+                else:
+                    return render_template('login.html', error='Wrong email or password')
             else:
-                user = User.get(User.email == request.form.get('email', None), User.password == request.form.get('password', None))
-                session['id'] = user.id
-                session['email'] = user.email
-                return redirect(url_for('notes'))
-        except User.DoesNotExist, e:
-            return render_template('login.html', error='Wrong email or password')
+                render_template('login.html', error='Email or password missing')
     else:
         return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+            if is_logged_in():
+                return redirect(url_for('notes'))
+            elif request.form.get('email', None) and request.form.get('password', None):
+                if user_exists(request.form.get('email')):
+                    return render_template('register.html', error='Email already in use')
+                else:
+                    user = User(email=request.form.get('email'), password=request.form.get('password'))
+                    user.save()
+                    session_login(user)
+                    return redirect(url_for('notes'))
+            else:
+                render_template('register.html', error='Email or password missing')
+    else:
+        return render_template('register.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'),404
 
 
+#FUNCTIONS
+
+def session_login(user):
+    session['id'] = user.id
+    session['email'] = user.email
+
+
 def is_logged_in():
-    return if 'username' in session and 'id' in session
+    return True if 'email' in session and 'id' in session else False
+        
+def user_exists(email):
+    try:
+        user = User.get(User.email == email)
+        return user
+    except User.DoesNotExist, e:
+        return None
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key = "SECRET"
     app.run()
